e6ef5599ce13fa2d93143c0dc53df8c81e97c89e
akshaychavhan <akshay@rtledgers.com>
2023-11-25 14:13:30 +0530

    05_ContextApi02 Themechanger

---
+++ 05_contextapi02_themechanger/src/context/Theme.js
@@ -0,0 +1,18 @@
+import { createContext, useContext } from "react";
+
+
+const ThemeContext = createContext({
+    themeMode : "light",
+    darkTheme : () => {},
+    lightTheme : () => {}
+})
+
+
+
+export const ThemeProvider = ThemeContext.Provider;
+
+
+export default function useTheme(){
+    return useContext(ThemeContext)
+}
+
